78ea448058991d191cef3dec8867e50dd144d227
Liz Nacho <lizjael.nap@gmail.com>
2026-06-14 13:02:37 -0400

    style: agregar estilos globales y paleta de colores Expreso Tupiza

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ToastContainer } from "react-toastify";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+import "./styles/global.css"; // <-- AGREGAR ESTA LÍNEA
 
 import { AuthProvider } from "./contexts/AuthContext";
 import { AppRouter } from "./routes/AppRouter";
